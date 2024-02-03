e79d48adf50156dfe3ea6b5edce078e5d3af5bc9
jDanek <jirka.danek@jdanek.eu>
2024-02-03 18:18:53 +0100

    [WIP] Update compatibility for SL 8.1

--- plugins/extend/hcm-content/class/ConfigAction.php
+++ plugins/extend/hcm-content/class/ConfigAction.php
@@ -4,6 +4,7 @@ namespace SunlightExtend\HcmContent;
 
 use Sunlight\Plugin\Action\ConfigAction as BaseConfigAction;
 use Sunlight\Util\Form;
+use Sunlight\Util\Request;
 
 class ConfigAction extends BaseConfigAction
 {
@@ -14,13 +15,13 @@ class ConfigAction extends BaseConfigAction
         return [
             'page_template' => [
                 'label' => _lang('hcm-content.config.page_template'),
-                'input' => '<textarea name="page_template" class="areasmallwide">' . Form::restorePostValue('page_template', $config['page_template'], false) . '</textarea>'
+                'input' => '<textarea name="page_template" class="areasmallwide">' . Request::post('page_template', $config['page_template']) . '</textarea>'
                 . '<p class="hint">' . _lang('hcm-content.config.page_template.hint') . '</p>',
                 'type' => 'text',
             ],
             'article_template' => [
                 'label' => _lang('hcm-content.config.article_template'),
-                'input' => '<textarea name="article_template" class="areasmallwide">' . Form::restorePostValue('article_template', $config['article_template'], false) . '</textarea>'
+                'input' => '<textarea name="article_template" class="areasmallwide">' . Request::post('article_template', $config['article_template']) . '</textarea>'
                 . '<p class="hint">' . _lang('hcm-content.config.article_template.hint') . '</p>',
                 'type' => 'text',
             ]
