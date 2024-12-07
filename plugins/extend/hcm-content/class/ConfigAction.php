1bfea7281c6bd379e28d9e975a2396b32d1c11d1
jDanek <jirka.danek@jdanek.eu>
2024-12-07 18:13:47 +0100

    Update version. Replace inputs, textareas and selects with system Form class methods

--- plugins/extend/hcm-content/class/ConfigAction.php
+++ plugins/extend/hcm-content/class/ConfigAction.php
@@ -15,16 +15,16 @@ class ConfigAction extends BaseConfigAction
         return [
             'page_template' => [
                 'label' => _lang('hcm-content.config.page_template'),
-                'input' => '<textarea name="page_template" class="areasmallwide">' . Request::post('page_template', $config['page_template']) . '</textarea>'
-                . '<p class="hint">' . _lang('hcm-content.config.page_template.hint') . '</p>',
+                'input' => Form::textarea('page_template', Request::post('page_template', $config['page_template']), ['class' => 'areasmallwide',])
+                    . '<p class="hint">' . _lang('hcm-content.config.page_template.hint') . '</p>',
                 'type' => 'text',
             ],
             'article_template' => [
                 'label' => _lang('hcm-content.config.article_template'),
-                'input' => '<textarea name="article_template" class="areasmallwide">' . Request::post('article_template', $config['article_template']) . '</textarea>'
-                . '<p class="hint">' . _lang('hcm-content.config.article_template.hint') . '</p>',
+                'input' => Form::textarea('article_template', Request::post('article_template', $config['article_template']), ['class' => 'areasmallwide',])
+                    . '<p class="hint">' . _lang('hcm-content.config.article_template.hint') . '</p>',
                 'type' => 'text',
-            ]
+            ],
         ];
     }
 }
